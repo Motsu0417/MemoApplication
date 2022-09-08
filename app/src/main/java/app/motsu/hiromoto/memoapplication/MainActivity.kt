6ca99205efb395b403aae1ba126980aa67bdd4fa
Motsu0417 <pippi9400@gmail.com>
2022-09-08 13:49:53 +0900

    Jsonを利用しての読み込みに変更

--- app/src/main/java/app/motsu/hiromoto/memoapplication/MainActivity.kt
+++ app/src/main/java/app/motsu/hiromoto/memoapplication/MainActivity.kt
@@ -6,6 +6,10 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.OnBackPressedCallback
 import app.motsu.hiromoto.memoapplication.databinding.ActivityMainBinding
+import org.json.JSONArray
+import org.json.JSONException
+import org.json.JSONObject
+import java.io.*
 
 const val TAG = "debug"
 
@@ -17,23 +21,49 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater).apply {setContentView(this.root)}
 
-        val savedStrData = getSharedPreferences("ListData", Context.MODE_PRIVATE).getString("JSON_DATA", null)
-        if(!savedStrData.isNullOrEmpty()){
-            Log.d(TAG, "data = $savedStrData")
-            items.clear()
-            savedStrData.split(';').forEach{
-                if(!it.isNullOrBlank()){
-                    val tmpArray = it.split(':')
-                    items.add(Item(tmpArray[0],tmpArray[1]))
+        // jsonの読み込み
+        val assetManager = resources.assets
+        try {
+//            val inputStream = assetManager.open("ListData.json")
+            val filepath = filesDir.path + "/ListData.json"
+            val file = File(filepath)
+            // ファイルが存在していれば読み込みを行う
+            if(file.exists()){
+                val bufferedReader = BufferedReader(FileReader(file))
+                val str = bufferedReader.readText()
+                // JsonArrayに格納する
+                val jsonArray = JSONArray(str)
+                for (i in 0 until jsonArray.length()) {
+                    jsonArray.getJSONObject(i).let {
+                        items.add(Item(it.getString("title"), it.getString("memo")))
+                    }
                 }
-            }
-            Log.d(TAG, "list = $items")
-        }else{
+            }else{
             Log.d(TAG, "Saved data is nothing")
         }
-//        itemArray.addAll((Array<Item>(10){Item().apply { title = "item $it" }}))
+
+        }catch(e:Exception){
+            Log.e(TAG, "onCreate: error", e)
+        }
 
 
+        // SharedPrefrencesを利用したデータの読み込み
+//        val savedStrData = getSharedPreferences("ListData", Context.MODE_PRIVATE).getString("JSON_DATA", null)
+//        if(!savedStrData.isNullOrEmpty()){
+//            Log.d(TAG, "data = $savedStrData")
+//            items.clear()
+//            savedStrData.split(';').forEach{
+//                if(!it.isNullOrBlank()){
+//                    val tmpArray = it.split(':')
+//                    items.add(Item(tmpArray[0],tmpArray[1]))
+//                }
+//            }
+//            Log.d(TAG, "list = $items")
+//        }else{
+//            Log.d(TAG, "Saved data is nothing")
+//        }
+
+        // リスト用フラグメントへの遷移
         supportFragmentManager.beginTransaction().apply{
             replace(R.id.edit_fragment_container,
             RecyclerViewFragment(this@MainActivity),
@@ -46,25 +76,54 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onStop() {
-        var saveData = ""
-        items.forEach{
-            saveData += "${it};"
+
+        // jsonの書き出し
+        try{
+            val filepath = filesDir.path + "/ListData.json"
+            val file = File(filepath)
+            val fileWriter = FileWriter(file, false)
+            val bufferdWriter = BufferedWriter(fileWriter)
+
+            val jsonArray = JSONArray()
+            items.forEach{
+                val jsonObject = JSONObject()
+                jsonObject.put("title", it.title)
+                jsonObject.put("memo", it.memo)
+                jsonArray.put(jsonObject)
+            }
+            Log.d(TAG, "onStop: $jsonArray")
+            bufferdWriter.write(jsonArray.toString())
+            bufferdWriter.close()
+        }catch(e:Exception){
+            Log.e(TAG, "onStop: ", e)
         }
-        Log.d(TAG, "onStop: \n$saveData")
-        getSharedPreferences("ListData", Context.MODE_PRIVATE).edit().apply{
-            putString("JSON_DATA", saveData).apply()
-        } ?: throw Exception("Can't get SharedPreferences")
+
+
+
+        // SharedPreferencesを利用したデータ保存
+//        var saveData = ""
+//        items.forEach{
+//            saveData += "${it};"
+//        }
+//        Log.d(TAG, "onStop: \n$saveData")
+//        getSharedPreferences("ListData", Context.MODE_PRIVATE).edit().apply{
+//            putString("JSON_DATA", saveData).apply()
+//        } ?: throw Exception("Can't get SharedPreferences")
+
         super.onStop()
     }
 
     override fun onBackPressed() {
-
+        // 「戻る」ボタンを推した際にRecyclerFragmentに戻るよう設定
+        // 元々RecyclerFragmentならそのまま終了
         if(supportFragmentManager.findFragmentById(R.id.edit_fragment_container)!!.tag != "RecyclerFragment"){
             supportFragmentManager.beginTransaction().apply{
                 replace(R.id.edit_fragment_container,
                     RecyclerViewFragment(this@MainActivity),
                     "RecyclerFragment")
             }.commit()
+
+
         }else{
             super.onBackPressed()
         }
